f3dcc293d7dc14661e995c72e16d519b53108ed5
naresh <nareshnalla567@gmail.com>
2020-07-12 00:22:24 +0530

    ok

--- Day/src/Days.java
+++ Day/src/Days.java
@@ -4,6 +4,10 @@ public class Days {
 	public static void main(String[] args) {
 		System.out.println("monday");
 		System.out.println("tuesday");
+		System.out.println("wednesday");
+		System.out.println("thuresday");
+		System.out.println("friday");
+		
 	}
 	}
 
